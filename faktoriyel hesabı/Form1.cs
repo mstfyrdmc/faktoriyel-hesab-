bbc079d5394839dfc6a3443cb9284eb4eb6d420c
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:00:08 +0300

    FaktoriyelHesabı

---
+++ faktoriyel hesabı/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace faktoriyel_hesabı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt16(textBox1.Text);
+            int faktoriyel=1;
+            for(int i=1;i<=sayi;i++)
+            {
+                faktoriyel = faktoriyel * i;
+            }
+            listBox1.Items.Add(faktoriyel);
+        }
+    }
+}
